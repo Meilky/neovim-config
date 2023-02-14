18a5a011cca00feb0b6e0a130acfa1c7e8b7076d
Meilky <mikaeljean2k3@gmail.com>
2023-02-14 17:15:54 -0500

    Update: Add eslint sources to null-ls and change sumenko_lus to lua_ls

--- lua/config/mason-lspconfig.lua
+++ lua/config/mason-lspconfig.lua
@@ -9,10 +9,10 @@ require("mason-lspconfig").setup_handlers({
 		})
 	end,
 
-	["sumneko_lua"] = function()
+	["lua_ls"] = function()
 		local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
-		require("lspconfig").sumneko_lua.setup({
+		require("lspconfig").lua_ls.setup({
 			capabilities = capabilities,
 			settings = {
 				Lua = {
