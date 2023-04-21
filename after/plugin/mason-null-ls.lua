07edec347722e216f29eea3ef20ba0445dbcdcf9
Meilky <mikaeljean2k3@gmail.com>
2023-04-21 08:52:16 -0400

    Update config to remove errors

--- after/plugin/mason-null-ls.lua
+++ after/plugin/mason-null-ls.lua
@@ -1,12 +1,9 @@
 local null_ls = require("null-ls");
 
-require("mason-null-ls").setup()
-
-require 'mason-null-ls'.setup_handlers({
-	function(source_name, methods)
-		require('mason-null-ls.automatic_setup')(source_name, methods)
-	end,
-	prettier = function (source_name, methods)
-		null_ls.register(null_ls.builtins.formatting.prettier)
-	end,
+require("mason-null-ls").setup({
+	handlers = {
+		prettier = function(source_name, methods)
+			null_ls.register(null_ls.builtins.formatting.prettier)
+		end,
+	}
 })
