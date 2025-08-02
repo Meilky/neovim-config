cc03ecba43fed28c68e106414d09b390a3c1b145
Meilky <mikaeljean2k3@gmail.com>
2025-08-02 10:33:47 -0400

    feat: remove individual mini plugin for mini.nvim

--- init.lua
+++ init.lua
@@ -15,21 +15,15 @@ vim.o.mouse = ""
 vim.o.winborder = "rounded"
 vim.g.mapleader = " "
 
-vim.keymap.set("n", "<A-n>", ":tabnew<CR>")
-vim.keymap.set("n", "<A-t>", ":tabnew<CR>:term<CR>i")
+vim.keymap.set("n", "<A-t>", ":tabnew<CR>")
 vim.keymap.set("n", "<A-h>", ":tabprevious<CR>")
 vim.keymap.set("n", "<A-l>", ":tabnext<CR>")
-vim.keymap.set("n", "<A-H>", ":tabmove -<CR>")
-vim.keymap.set("n", "<A-L>", ":tabmove +<CR>")
-vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]])
+vim.keymap.set("n", "<A-j>", ":tabmove -<CR>")
+vim.keymap.set("n", "<A-k>", ":tabmove +<CR>")
 
 vim.pack.add({
 	{ src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "main" },
-	{ src = "https://github.com/echasnovski/mini.pick" },
-	{ src = "https://github.com/echasnovski/mini.surround" },
-	{ src = "https://github.com/echasnovski/mini.completion" },
-	{ src = "https://github.com/echasnovski/mini.icons" },
-	{ src = "https://github.com/echasnovski/mini.ai" },
+	{ src = "https://github.com/echasnovski/mini.nvim" },
 	{ src = "https://github.com/stevearc/oil.nvim" },
 	{ src = "https://github.com/mason-org/mason.nvim" },
 	{ src = "https://github.com/mason-org/mason-lspconfig.nvim" },
@@ -44,7 +38,10 @@ vim.keymap.set("n", "<leader>o", ":Oil<CR>")
 vim.keymap.set("n", "<A-o>", ":tabnew<CR>:Oil<CR>")
 
 require('mini.surround').setup()
+require('mini.git').setup()
+require('mini.diff').setup()
 require('mini.completion').setup()
+require('mini.statusline').setup()
 require("mini.ai").setup({
 	search_method = "cover",
 })
